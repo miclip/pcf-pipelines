86cfe7787cc130b57f9d77fa624fce88598449c7
Kartik Lunkad <klunkad@pivotal.io>
2018-06-15 18:20:38 -0400

    Additional commands

--- deploy-pasw/tasks/build-injected-pasw/task.sh
+++ deploy-pasw/tasks/build-injected-pasw/task.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eu
 
-tar -xvf winfs-injector-0.8.0.zip
+echo "Executing the script now"
+
+ls -ltr
+
+tar -xvf ./winfs-injector-0.8.0.zip
 chmod 700 winfs-injector-linux
 ./winfs-injector-linux --input-tile pas-windows-2.1.6-build.1.pivotal --output-tile pas-windows-2.1.6.injected.pivotal
