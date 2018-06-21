94c242385e4539e456d810a048d0ff4a5c4c7284
Kartik Lunkad <klunkad@pivotal.io>
2018-06-21 13:14:01 -0400

    Updates

--- deploy-pasw/tasks/build-injected-pasw/task.sh
+++ deploy-pasw/tasks/build-injected-pasw/task.sh
@@ -12,4 +12,6 @@ ls -ltr
 unzip ./winfs-injector-0.8.0.zip
 chmod 700 winfs-injector-linux
 
+ls -ltr ../pivnet-pasw/
+
 ./winfs-injector-linux --input-tile ../pivnet-pasw/pas-windows-2.1.6-build.1.pivotal --output-tile ../pasw-injected/pas-windows-2.1.6.injected.pivotal
