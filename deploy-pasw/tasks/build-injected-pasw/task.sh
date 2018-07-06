7237fcb2fec03d492cdcb5f56ffd63f0e970e74c
Kartik Lunkad <klunkad@pivotal.io>
2018-07-06 12:24:30 -0400

    Message

--- deploy-pasw/tasks/build-injected-pasw/task.sh
+++ deploy-pasw/tasks/build-injected-pasw/task.sh
@@ -8,12 +8,16 @@ cd pivnet-winfs-injector
 unzip "*winfs*"
 chmod 700 winfs-injector-linux
 
+ls -ltr
 #Find the original tile
-FILE_PATH=`find ../pivnet-pasw -name *.pivotal | sort | head -1`
+cd ../pivnet-pasw
+FILE_PATH=`find . -name *.pivotal | sort | head -1`
 
-echo $FILE_PATH
+cd ..
+
+ls -ltr
 
 #Run the injector with the input as the original tile and output the winfs-injected tile
-./winfs-injector-linux --input-tile ../pivnet-pasw/$FILE_PATH --output-tile ../pasw-injected/$FILE_PATH
+#./winfs-injector-linux --input-tile ../pivnet-pasw/$FILE_PATH --output-tile ../pasw-injected/$FILE_PATH
 
-ls -ltr ../pasw-injected
+#ls -ltr ../pasw-injected
